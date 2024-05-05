c84b784504a61b0f0a00cea59b71466a0c7fa634
tristar <tristar.2nd@gmail.com>
2024-05-06 02:49:10 +0900

    実際にリクエストを送信する対応

--- packages/admin/src/server/api/auth/login.post.ts
+++ packages/admin/src/server/api/auth/login.post.ts
@@ -1,8 +1,30 @@
 import { defineEventHandler } from 'h3'
+import { z } from 'zod'
+import axios from 'axios'
+
+const loginRequestSchema = z.object({
+  loginId: z.string(),
+  password: z.string(),
+})
+
+const loginResponseSchema = z.object({
+  token: z.string(),
+})
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  console.log('body', body)
-  event.node.res.statusCode = 401
+  const requestJson = loginRequestSchema.parse(body)
+
+  try {
+    const response = await axios.post('/admin/auth/login', requestJson)
+    return loginResponseSchema.parse(response.data)
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      if (e.status) {
+        event.node.res.statusCode = e.status
+      }
+    } else {
+      event.node.res.statusCode = 500
+    }
+  }
 })

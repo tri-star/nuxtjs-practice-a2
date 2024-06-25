60358a373a9efff980c7e4dc248a7b5f33a8d615
tristar <tristar.2nd@gmail.com>
2024-06-26 04:38:20 +0900

    型エラー修正

--- packages/admin/src/features/auth/api/validate-admin-user-login-id.ts
+++ packages/admin/src/features/auth/api/validate-admin-user-login-id.ts
@@ -2,11 +2,26 @@ import { err, ok, type Result } from 'neverthrow'
 import { createAppApiClient } from '~/lib/api/client'
 import { toAppErrorOrThrow, type ApplicationError } from '~/lib/error/app-error'
 
-export async function validateAdminUserLoginId(loginId: string): Promise<Result<boolean, ApplicationError>> {
+type ValidateLoginIdRequest = {
+  loginId: string
+  excludeSelf?: string
+}
+
+export async function validateAdminUserLoginId(
+  loginId: string,
+  excludeSelf: boolean = false,
+): Promise<Result<boolean, ApplicationError>> {
   try {
+    const queries: ValidateLoginIdRequest = {
+      loginId,
+    }
+    if (excludeSelf) {
+      queries['excludeSelf'] = ''
+    }
+
     const client = createAppApiClient()
     const response = await client.get('/admin/auth/validate-login-id', {
-      queries: { loginId },
+      queries: queries,
     })
     return ok(response.valid)
   } catch (e) {
